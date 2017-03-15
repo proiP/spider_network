91db2d853d77702e5f65fcf7355794ba637f1ce8
proiP <froilangaviola@gmail.com>
2017-03-15 17:53:45 +0800

    added bower, installed semanticUI

--- spider_network/settings.py
+++ spider_network/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
+    'djangobower',
+    'djangobower.finders.BowerFinder',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+BOWER_COMPONENTS_ROOT = '/spider_network/components/'
