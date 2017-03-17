5ec1a86eeee0f10dcde184084d23ed2df02c02f9
proiP <froilangaviola@gmail.com>
2017-03-17 10:04:00 +0800

    testing the ui

--- base/views.py
+++ base/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+import nmap
 
 # Create your views here.
 
 def home_view(request):
     template = 'body/home.html'
+    nm = nmap.PortScanner()
+    print nm
     return render(request, template)
     
